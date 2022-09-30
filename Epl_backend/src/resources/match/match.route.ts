2fb1839517b8e4b2df0be35ed88cd6fdb1469f86
zerihunttt655674 <zeerin4an@gmail.com>
2022-09-30 19:05:04 +0300

    limit and page to query

--- Epl_backend/src/resources/match/match.route.ts
+++ Epl_backend/src/resources/match/match.route.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
-import { getMatch, getMatches, updateMatch } from './match.controller'
+import {
+  getLiveMatches,
+  getMatch,
+  getMatches,
+  getResult,
+  updateMatch
+} from './match.controller'
 
 const matchRouter = Router()
-
+matchRouter.get('/results', getResult)
+matchRouter.get('/live', getLiveMatches)
 matchRouter.get('', getMatches)
 matchRouter.get('/:id', getMatch)
 matchRouter.put('', updateMatch)
-matchRouter.put('/:match', updateMatch)
+// matchRouter.put('/:match', updateMatch)
 
 export default matchRouter
